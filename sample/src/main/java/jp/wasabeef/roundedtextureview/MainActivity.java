9158a914a38f5d5efa7d8c16c185e76bef78bf19
rqg <ranqingguo90@qq.com>
2017-10-12 17:23:26 +0800

    round corner work

--- sample/src/main/java/jp/wasabeef/roundedtextureview/MainActivity.java
+++ sample/src/main/java/jp/wasabeef/roundedtextureview/MainActivity.java
@@ -1,14 +1,31 @@
 package jp.wasabeef.roundedtextureview;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    private RoundedTextureView mRoundedTextureView;
+    private MediaPlayer mPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRoundedTextureView = (RoundedTextureView) findViewById(R.id.round_video);
+
+        mRoundedTextureView.setCornerRadius(20);
+
+        mPlayer = MediaPlayer.create(this, R.raw.sample);
+
+        mRoundedTextureView.setSurfaceProvider(surface -> {
+            mPlayer.setSurface(new Surface(surface));
+            mPlayer.start();
+        });
+    }
 }
